e827a75eff5a8198f9c7a2ee73fdad8ebf24c01a
xuegang <xuegang20@163.com>
2021-01-29 16:55:00 +0800

    Spring框架 HelloWord搭建

---
+++ code/SpringExample/src/com/tutorialspoint/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.tutorialspoint;
+
+public class HelloWorld {
+    private String message;
+
+    public void getMessage() {
+        System.out.println("your message: " + message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
